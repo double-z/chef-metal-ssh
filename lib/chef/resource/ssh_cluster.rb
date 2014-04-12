ad0f1e47e92261ecffeaea3b95c3ccf9d26cd592
double-z <zzondlo@carecloud.com>
2014-04-12 11:49:24 -0400

    gimme da cluster path por favor

--- lib/chef/resource/ssh_cluster.rb
+++ lib/chef/resource/ssh_cluster.rb
@@ -2,15 +2,24 @@ require 'chef/resource/lwrp_base'
 require 'chef_metal_ssh'
 
 class Chef::Resource::SshCluster < Chef::Resource::LWRPBase
-  self.resource_name = 'ssh_cluster'
+ self.resource_name = 'ssh_cluster'
 
-  actions :create, :delete, :nothing
-  default_action :create
+ actions :create, :delete, :nothing
+ default_action :create
 
-  attribute :path, :kind_of => String, :name_attribute => true
+ attribute :path, :kind_of => String, :name_attribute => true
 
-  def after_created
-    super
-    ChefMetal.with_ssh_cluster path
-  end
+ def after_created
+   super
+   ChefMetal.with_ssh_cluster path
+ end
+ 
+ def self.path=(_path)
+   @@path = _path 
+ end
+ 
+ def self.path
+   @@path
+ end
+   
 end
