3b64276a22ad5841bd3d6eeaedda042e2a7047ba
double-z <zackzondlo@gmail.com>
2014-10-12 11:45:30 -0400

    now working with metal ~> 0.12

--- test/cookbooks/ssh_test/recipes/both.rb
+++ test/cookbooks/ssh_test/recipes/both.rb
@@ -1,29 +1,33 @@
 require 'chef_metal'
-require 'chef_metal_ssh/ssh_provisioner'
+require 'chef_metal_ssh'
+
+# with_driver 'ssh'
+ssh_cluster_path = "/vagrant/test/ssh_cluster"
+ssh_cluster ssh_cluster_path
+
+with_ssh_cluster(ssh_cluster_path)
 
 # require 'chef/config'
 # with_chef_server "http://192.168.33.27:6900", {
 #   :client_name => Chef::Config[:node_name],
 #   :signing_key_filename => Chef::Config[:client_key]
 # }
-include_recipe 'ssh_test::register_target'
+# include_recipe 'ssh_test::register_target'
 
 with_chef_local_server :chef_repo_path => "/vagrant/test",
   :port => "8900"
 
 machine "one" do
-  # action [:create, :converge]
-  action :create
+  action [:ready, :converge]
+  # action :ready
   # action :converge
-  converge true
-  provisioner ChefMetalSsh::SshProvisioner.new
-  provisioner_options "ssh_options" => {
-                        'user' => 'vagrant'
-                      },
-                      'machine_options' => {
-                        'ip_address' => '192.168.33.21',
-                        'password' => 'vagrant'
-                      }
+  # converge true
+  # provisioner ChefMetalSsh::SshProvisioner.new
+  machine_options :ip_address => '192.168.33.21',
+                  :ssh_options => {
+                    :user => 'vagrant',
+                    :password => 'vagrant'
+                  }
   recipe 'ssh_test::remote1'
 end
 
@@ -48,43 +52,43 @@ end
 #   action :nothing
 # end
 
-# ##
-# # Machine Two
-machine "two" do
-  action :create
-  # action :nothing
-  converge true
-  provisioner ChefMetalSsh::SshProvisioner.new
-  provisioner_options "ssh_options" => {
-                        'user' => 'vagrant'
-                      },
-                      'machine_options' => {
-                        'ip_address' => '192.168.33.22',
-                        'machine_types' => ['app_server', 'web_server'],
-                        'password' => 'vagrant'
-                      }
-  recipe 'ssh_test::remote1'
-  # recipe 'ssh_test::remote2'
-  # notifies :run, 'execute[run_touch2]'
-end
+# # ##
+# # # Machine Two
+# machine "two" do
+#   action :create
+#   # action :nothing
+#   converge true
+#   provisioner ChefMetalSsh::SshProvisioner.new
+#   provisioner_options "ssh_options" => {
+#                         'user' => 'vagrant'
+#                       },
+#                       'machine_options' => {
+#                         'ip_address' => '192.168.33.22',
+#                         'machine_types' => ['app_server', 'web_server'],
+#                         'password' => 'vagrant'
+#                       }
+#   recipe 'ssh_test::remote1'
+#   # recipe 'ssh_test::remote2'
+#   # notifies :run, 'execute[run_touch2]'
+# end
 
-machine "three" do
-  action :create
-  # action :nothing
-  # converge true
-  provisioner ChefMetalSsh::SshProvisioner.new
-  provisioner_options "ssh_options" => {
-                        'user' => 'vagrant'
-                      },
-                      'machine_options' => {
-                        'ip_address' => '192.168.33.22',
-                        'machine_types' => ['app_server', 'web_server'],
-                        'password' => 'vagrant'
-                      }
-  recipe 'ssh_test::remote1'
-  # recipe 'ssh_test::remote2'
-  # notifies :run, 'execute[run_touch2]'
-end
+# machine "three" do
+#   action :create
+#   # action :nothing
+#   # converge true
+#   provisioner ChefMetalSsh::SshProvisioner.new
+#   provisioner_options "ssh_options" => {
+#                         'user' => 'vagrant'
+#                       },
+#                       'machine_options' => {
+#                         'ip_address' => '192.168.33.22',
+#                         'machine_types' => ['app_server', 'web_server'],
+#                         'password' => 'vagrant'
+#                       }
+#   recipe 'ssh_test::remote1'
+#   # recipe 'ssh_test::remote2'
+#   # notifies :run, 'execute[run_touch2]'
+# end
 
 
 # execute 'run_touch2' do
